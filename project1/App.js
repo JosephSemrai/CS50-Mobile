86e9e9810a7e1fb287e14cae8234cc3fb99916db
Joseph Semrai <josephsemrai@gmail.com>
2019-08-04 17:58:49 -0400

    Full app

--- project1/App.js
+++ project1/App.js
@@ -3,19 +3,21 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import {Timer, vibrate} from './utils'
 import {Countdown} from './components'
 
-
-
+const DEFAULT_WORK_TIME = 1500000; // 25 min
+const DEFAULT_BREAK_TIME = 300000; // 5 min
 
 export default class App extends React.Component {
 
   state = {
-    timeRemaining: 1000,
+    timeRemaining: DEFAULT_WORK_TIME, // Sets the initial time that the timer will be set to, this will be updated later
+    timerMode: "Work",
+    timerStopped: true,
+    toggleTimerButtonText: "Start Timer",
   };
 
   componentDidMount() {
     console.log("component mounted");
-    this.timer = new Timer(this.state.timeRemaining, this.updateRemainingTime, () => {console.log("the end function was called from the timer")})
-
+    this.timer = new Timer(this.state.timeRemaining, this.updateRemainingTime, this.timerEnded);
   }
 
   // This is passed into the timer instance which is called every time we get a new time
@@ -23,13 +25,84 @@ export default class App extends React.Component {
     this.setState({timeRemaining: remainingTime});
   };
 
+  timerEnded = () => {
+    vibrate();
+    this.timer.stop();
+    this.switchModes();
+  };
+
+  switchModes = () => {
+    if (this.state.timerMode === "Work") {
+      this.breakMode();
+    }
+    else
+    {
+      this.workMode();
+    }
+  };
+
+  workMode = () => {
+    this.setState({
+      timeRemaining: DEFAULT_WORK_TIME,
+      timerMode: "Work"
+    }, () => {
+      this.timer = new Timer(this.state.timeRemaining, this.updateRemainingTime, this.timerEnded);
+      this.setState({toggleTimerButtonText: "Start Timer"});
+    });
+  };
+
+  breakMode = () => {
+    this.setState({
+      timeRemaining: DEFAULT_BREAK_TIME,
+      timerMode: "Break"
+    }, () => {
+      this.timer = new Timer(this.state.timeRemaining, this.updateRemainingTime, this.timerEnded);
+      this.setState({toggleTimerButtonText: "Start Timer"});
+    });
+
+  };
+
+  resetTimer = () => {
+    console.log("reset timer")
+    if (this.state.timerMode == "Work") {
+      this.timer.stop();
+      this.workMode();
+    }
+    else {
+      this.timer.stop();
+      this.breakMode();
+    }
+  };
+
+  toggleTimer = () => {
+    if (this.timer.isRunning) {
+      this.timer.stop();
+      this.setState({toggleTimerButtonText: "Start Timer"});
+    } 
+    else {
+      this.timer.start();
+      this.setState({toggleTimerButtonText: "Stop Timer"});
+    }
+  };
+
+
 
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={styles.titleText}>{this.state.timerMode}</Text>
         <Countdown timeRemaining={this.state.timeRemaining} />
+
+        <Button 
+          title={this.state.toggleTimerButtonText}
+          onPress={() => this.toggleTimer()}
+        />
+
+        <Button 
+          title="Reset Timer"
+          onPress={() => this.resetTimer()}
+        />
       </View>
     );
   }
@@ -43,8 +116,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  timerText: {
-    fontSize: 27,
+  titleText: {
+    fontSize: 50,
     fontWeight: 'bold',
   },
 });
--- project1/utils/timer.js
+++ project1/utils/timer.js
@@ -4,20 +4,42 @@ export default class Timer {
         this.duration = duration;
         this.tickFunction = tickFunction;
         this.endFunction = endFunction;
-        this.endTime = Date.now() + duration;
-        this.tick(); //starts the first tick after being constructed
+        this.isRunning = false;
     }
 
-    get timeRemaining() {
-        return this.endTime - Date.now();
-    };
+
 
     tick = () => {
-        console.log(this.timeRemaining);
-        this.tickFunction(this.timeRemaining) //passes back and updates app.js with current time
-
-        // accounts for lag by only running on the next round second and will now only call tick every second
-        tickDelay = this.timeRemaining % 1000;
-        this.timeout = setTimeout(this.tick, tickDelay)
+        if (this.duration < 1000) {
+            console.log("timer ended!");
+            this.tickFunction(0);
+            this.endFunction();
+        }
+        else {
+            console.log(this.duration);
+            this.duration = this.duration - 1000;
+            this.tickFunction(this.duration); //passes back and updates app.js with current time
+    
+            // accounts for lag by only running on the next round second and will now only call tick every second
+            tickDelay = this.duration % 1000;
+            this.timeout = setTimeout(this.tick, 1000 + tickDelay);
+        }
     }
+
+    start = () => {
+        if (this.isRunning) {
+            console.log("can't start timer, timer already running")
+            return;
+        }
+        else {
+            this.isRunning = true;
+            this.tick();
+        }
+    };
+
+    stop = () => {
+        this.isRunning = false;
+        clearTimeout(this.timeout);
+        this.timeout = null;
+    };
 }
